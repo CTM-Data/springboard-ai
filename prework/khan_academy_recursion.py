ebfa43b980be0c02752252ffd37de0d5e813dcd0
Clay Marquardt <clayton.marquardt@gdit.com>
2024-08-08 16:00:33 -0400

    added functions to recursion file

--- prework/khan_academy_recursion.py
+++ prework/khan_academy_recursion.py
@@ -12,3 +12,32 @@ fac0 = 1
 fac0func = factorial(0)
 print(f'0! the same: {fac0 == fac0func}')
 
+
+def recursive_factorial(n):
+    # base case: 
+    if n == 0 or n == 1: 
+        print(1)
+    # recursive case: 
+    else:
+        print(n * factorial(n-1))
+
+print(recursive_factorial(5))
+
+
+def is_palindrome(string):
+    # base case: 
+    if len(string) == 0 or len(string) == 1:
+        return True
+    if string[0] != string[-1]:
+        return False
+    elif string[0] == string[-1]:
+        substring = string[1:-1]
+        return is_palindrome(substring)
+    else:
+        return False
+
+word = 'taat'
+print(f'is {word} a palidrome?', is_palindrome(word))
+
+
+
